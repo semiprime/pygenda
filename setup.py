dab4ac65926c8b85436532d7ef574d7bdd402511
Matt Lewis <git@semiprime.com>
2023-07-03 23:50:02 +0200

    Setup/distribution. Fixes to files included in sdist & bdists (added MANIFEST.in).

--- setup.py
+++ setup.py
@@ -20,6 +20,13 @@
 # along with Pygenda. If not, see <https://www.gnu.org/licenses/>.
 #
 
+# Example usage:
+#   ./setup.py install [--user]
+#   ./setup.py develop [--user]
+#   ./setup.py sdist
+#   ./setup.py bdist_wheel --plat-name=[linux-x86_64|linux-aarch64]
+
+
 from setuptools import setup
 from setuptools.command.bdist_egg import bdist_egg
 from sys import platform
@@ -70,7 +77,7 @@ setup(
     long_description = "Pygenda is a calendar/agenda application written in Python3/GTK3. The UI is inspired by the Agenda programs on the Psion Series 3 and Series 5 range of keyboard PDAs, with the aim of being suitable for devices such as Planet Computers' Gemini (running Linux).\n\nFor more information/latest source, see https://github.com/semiprime/pygenda",
     packages = ["pygenda"],
     cmdclass = {'bdist_egg': PygendaEggInstall}, # use custom install above
-    package_data = {'': ['glade/*.glade', 'css/*.css', 'css/*.svg', 'libpygenda_clipboard.so', 'locale/*/LC_MESSAGES/pygenda.mo', '../docs/*.md', '../docs/config-examples/*.md', '../docs/config-examples/*.ini', '../docs/config-examples/*.css', '../docs/config-examples/pygenda.desktop']},
+    package_data = {'': ['glade/*.glade', 'css/*.css', 'css/*.svg', 'libpygenda_clipboard.so', 'locale/*/LC_MESSAGES/pygenda.mo']}, # files for bdists
     license = "GPLv3 only",
     python_requires = ">=3.5",
     setup_requires = ["pycairo"], # For PyGObject
