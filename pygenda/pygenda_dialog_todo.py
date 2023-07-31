e01a6d22202eaa439e2e20a16e3bb8372273c852
Matt Lewis <git@semiprime.com>
2023-07-31 23:28:14 +0200

    Todo dialog. Added error checking on "OK". However, if it's a new todo and nothing has been entered, just discard the entry.

--- pygenda/pygenda_dialog_todo.py
+++ pygenda/pygenda_dialog_todo.py
@@ -64,6 +64,7 @@ class TodoDialogController:
             raise NameError('Dialog Todo not found')
 
         cls.wid_desc = GUI._builder.get_object('entry_dialogtodo_desc')
+        cls.wid_desc.connect('changed', cls._validated_field_changed)
         cls._init_todolists()
         cls.wid_priority = GUI._builder.get_object('combo_todo_priority')
         cls.wid_status = GUI._builder.get_object('combo_todo_status')
@@ -107,13 +108,23 @@ class TodoDialogController:
     def _do_todo_dialog(cls, todo:iTodo=None, txt:str=None, list_idx:Optional[int]=0) -> Tuple[int,EntryInfo,int]:
         # Do the core work displaying todo dialog and extracting result.
         cls._seed_fields(todo=todo, txt=txt, list_idx=list_idx)
+        cls._reset_err_style() # clear error highlights
+        discard_on_empty = (todo is None) # True if creating a new todo entry
         try:
             while True:
                 response = cls.dialog.run()
-                break
+                if response!=Gtk.ResponseType.OK:
+                    break
+                if discard_on_empty and cls._fields_are_defaults():
+                    # New todo & nothing entered, so discard as a null entry
+                    response = Gtk.ResponseType.CANCEL
+                    break
+                if cls._is_valid_todo(set_style=True):
+                    break
+                discard_on_empty = False # only allow discard first time
         finally:
             cls.dialog.hide()
-        return response,cls._get_entryinfo(),cls.wid_todolist.get_active()
+        return response, cls._get_entryinfo(), cls.wid_todolist.get_active()
 
 
     @classmethod
@@ -155,6 +166,61 @@ class TodoDialogController:
         cls.wid_todolist.set_active(0)
         cls.wid_priority.set_active(0)
         cls.wid_status.set_active(0)
+
+
+    @classmethod
+    def _fields_are_defaults(cls) -> bool:
+        # Return True if all fields are defaults, False if any non-default
+        if cls.wid_desc.get_text():
+            return False
+        if cls.wid_priority.get_active()!=0:
+            return False
+        if cls.wid_status.get_active()!=0:
+            return False
+        return True
+
+
+    @classmethod
+    def _is_valid_todo(cls, set_style:bool=False) -> bool:
+        # Function checks if todo dialog fields are valid.
+        # Used when "OK" clicked in the dialog.
+        # Returns True if all fields valid; False if *any* field is invalid.
+        # If a field is invalid and set_style==True it also adds a
+        # CSS class to the widget, so the error is visibly indicated.
+        # If the event is valid then it removes the error style, so
+        # the error indication will disappear (regardless of set_style).
+
+        ret = True
+
+        # Check description is non-empty
+        ctx = cls.wid_desc.get_style_context()
+        if not cls.wid_desc.get_text():
+            ret = False
+            if set_style:
+                ctx.add_class(GUI.STYLE_ERR)
+        else:
+            ctx.remove_class(GUI.STYLE_ERR)
+
+        return ret
+
+
+    @classmethod
+    def _reset_err_style(cls) -> None:
+        # Remove 'dialog_error' style class from todo dialog widgets where
+        # it might be set, so any visible indications of errors are removed.
+        # Used, for example, when the dialog is reinitialised.
+        for w in (cls.wid_desc,):
+            w.get_style_context().remove_class(GUI.STYLE_ERR)
+
+
+    @classmethod
+    def _validated_field_changed(cls, wid:Gtk.Entry) -> bool:
+        # Callback. Called when a "validated field" changes.
+        # A "validated field" is one that is checked with _is_valid_todo().
+        # Function removes any error state styling that is no longer needed.
+        # It does *not* add any new error styling.
+        cls._is_valid_todo(set_style=False)
+        return False # propagate event
 
 
     @classmethod
